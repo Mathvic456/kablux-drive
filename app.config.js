530f430a682b2662be71701a6501ba0181ef86c2
Ago2iem <agoziemogbe@gmail.com>
2026-03-30 13:49:45 +0100

    2 complete 2/3 of the kyc process. build-android

--- app.config.js
+++ app.config.js
@@ -68,7 +68,7 @@ export default {
       ],
       package: "com.crashingout.kabluxdrive",
       googleServicesFile: "./google-services.json",
-      versionCode: 46,
+      versionCode: 47,
     },
     web: {
       favicon: "./assets/favicon.png",
