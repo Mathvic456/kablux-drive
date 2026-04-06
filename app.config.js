a6d1c65eb3a7976425230648b27ee95e1079de04
Ago2iem <agoziemogbe@gmail.com>
2026-04-06 18:34:18 +0100

    2 selfies retake added and success message for added photos. build-android

--- app.config.js
+++ app.config.js
@@ -68,7 +68,7 @@ export default {
       ],
       package: "com.crashingout.kabluxdrive",
       googleServicesFile: "./google-services.json",
-      versionCode: 48,
+      versionCode: 50,
     },
     web: {
       favicon: "./assets/favicon.png",
