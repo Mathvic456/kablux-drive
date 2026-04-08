7f333a754fefb5e2f163adde01e0b409f78bebdd
Ago2iem <agoziemogbe@gmail.com>
2026-04-08 08:47:21 +0100

    setting permissions. build-android

--- app.config.js
+++ app.config.js
@@ -68,7 +68,7 @@ export default {
       ],
       package: "com.crashingout.kabluxdrive",
       googleServicesFile: "./google-services.json",
-      versionCode: 50,
+      versionCode: 51,
     },
     web: {
       favicon: "./assets/favicon.png",
